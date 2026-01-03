f8874ca27aabe38cb17186bde4971b08af520044
ChetanAbhishek <chetanabhishek21@gmail.com>
2026-01-03 21:30:04 +0530

    Project to practice Branching

---
+++ tests/practice-page.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+  await page.locator('#checkBoxOption1').check();
+  await page.locator('#checkBoxOption2').check();
+  await page.getByRole('textbox', { name: 'Enter Your Name' }).click();
+  await page.getByRole('textbox', { name: 'Enter Your Name' }).fill('test1');
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Alert' }).click();
+  await page.getByRole('textbox', { name: 'Type to Select Countries' }).click();
+  await page.getByRole('textbox', { name: 'Type to Select Countries' }).fill('me');
+ // await page.locator('#ui-id-13').click();
+  await expect(page.getByRole('link', { name: 'Open Tab' })).toBeVisible();
+  await expect(page.locator('body')).toContainText('Dropdown Example');
+});
